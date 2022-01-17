de3220579e493d8d44b9c0e7a7e5e535e5a22b49
manamafashatheogene@gmail.com <manamfashatheogene@gmail.com>
2022-01-17 23:07:47 +0200

    appel des fichiers css et javascripts

--- CRDLPROJET/App_Start/BundleConfig.cs
+++ CRDLPROJET/App_Start/BundleConfig.cs
@@ -5,7 +5,6 @@ namespace CRDLPROJET
 {
     public class BundleConfig
     {
-        
         // Pour plus d’informations sur le Bundling, accédez à l’adresse http://go.microsoft.com/fwlink/?LinkId=254725 (en anglais)
         public static void RegisterBundles(BundleCollection bundles)
         {
