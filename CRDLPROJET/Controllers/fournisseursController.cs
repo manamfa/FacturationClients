9e71f68b8edb6653a2eea2d4fc65beff0c55314c
manamafashatheogene@gmail.com <manamfashatheogene@gmail.com>
2022-01-18 11:19:56 +0200

    ajout d'une fonction create pour inserer les Listes des fournisseurs

--- CRDLPROJET/Controllers/fournisseursController.cs
+++ CRDLPROJET/Controllers/fournisseursController.cs
@@ -39,8 +39,6 @@ namespace CRDLPROJET.Controllers
         {
             return View();
         }
-
-
         // POST: fournisseurs/Create
         // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
         // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
