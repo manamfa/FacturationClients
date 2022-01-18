e3fdf36b073df190b7b573635ddfca345abc8c58
manamafashatheogene@gmail.com <manamfashatheogene@gmail.com>
2022-01-18 19:04:38 +0200

    ajout d'une fonction Create pour Inserer l'utilisateur

--- CRDLPROJET/Controllers/usersController.cs
+++ CRDLPROJET/Controllers/usersController.cs
@@ -129,7 +129,6 @@ namespace CRDLPROJET.Controllers
             ViewBag.profileID = new SelectList(db.profils, "profileID", "profilename");
             return View();
         }
-
         // POST: users/Create
         // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
         // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
