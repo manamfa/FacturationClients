025222d61d6fd0d8e40824ec5e6d7fb4a4f93803
manamafashatheogene@gmail.com <manamfashatheogene@gmail.com>
2022-01-18 17:05:36 +0200

    ajout d'une fonction Edit pour Modifier historique des prix

--- CRDLPROJET/Controllers/historiqueprixesController.cs
+++ CRDLPROJET/Controllers/historiqueprixesController.cs
@@ -93,7 +93,6 @@ namespace CRDLPROJET.Controllers
             ViewBag.produitID = new SelectList(db.produits, "produitID", "nomproduit", historiqueprix.produitID);
             return View(historiqueprix);
         }
-
         // POST: historiqueprixes/Edit/5
         // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
         // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
